77403a63bfd83631ec2eccb784bd10b7ca3ca4e2
Janne Papula <raybarg@gmail.com>
2022-01-13 12:34:55 +0200

    Added tab control

--- frmWurmSermoner.Designer.cs
+++ frmWurmSermoner.Designer.cs
@@ -30,87 +30,33 @@ namespace WurmSermoner
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtOperator = new System.Windows.Forms.TextBox();
-            this.txtLogsDir = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtLogFile = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.btnStuff = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
             this.btnStop = new System.Windows.Forms.Button();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.tsBot = new System.Windows.Forms.ToolStripStatusLabel();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.tsLogFile = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btnStuff = new System.Windows.Forms.Button();
+            this.txtLogFile = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLogsDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOperator = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.txtList = new System.Windows.Forms.TextBox();
             this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 59);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(48, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Operator";
-            // 
-            // txtOperator
-            // 
-            this.txtOperator.Location = new System.Drawing.Point(148, 56);
-            this.txtOperator.Name = "txtOperator";
-            this.txtOperator.Size = new System.Drawing.Size(126, 20);
-            this.txtOperator.TabIndex = 1;
-            this.txtOperator.Text = "Tiscattu";
-            // 
-            // txtLogsDir
-            // 
-            this.txtLogsDir.Location = new System.Drawing.Point(148, 82);
-            this.txtLogsDir.Name = "txtLogsDir";
-            this.txtLogsDir.Size = new System.Drawing.Size(586, 20);
-            this.txtLogsDir.TabIndex = 3;
-            this.txtLogsDir.Text = "D:\\Games\\Steam\\steamapps\\common\\Wurm Online\\gamedata\\players\\Tiscattu\\logs\\";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(94, 85);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Logs dir";
-            // 
-            // txtLogFile
-            // 
-            this.txtLogFile.Location = new System.Drawing.Point(148, 108);
-            this.txtLogFile.Name = "txtLogFile";
-            this.txtLogFile.Size = new System.Drawing.Size(126, 20);
-            this.txtLogFile.TabIndex = 5;
-            this.txtLogFile.Text = "_Event.2022-01.txt";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(94, 111);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "Logfile";
-            // 
-            // btnStuff
-            // 
-            this.btnStuff.Location = new System.Drawing.Point(148, 134);
-            this.btnStuff.Name = "btnStuff";
-            this.btnStuff.Size = new System.Drawing.Size(83, 23);
-            this.btnStuff.TabIndex = 6;
-            this.btnStuff.Text = "Parse log";
-            this.btnStuff.UseVisualStyleBackColor = true;
-            this.btnStuff.Click += new System.EventHandler(this.btnStuff_Click);
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(426, 137);
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(652, 26);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 7;
@@ -120,7 +66,8 @@ namespace WurmSermoner
             // 
             // btnStop
             // 
-            this.btnStop.Location = new System.Drawing.Point(426, 166);
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Location = new System.Drawing.Point(652, 55);
             this.btnStop.Name = "btnStop";
             this.btnStop.Size = new System.Drawing.Size(75, 23);
             this.btnStop.TabIndex = 8;
@@ -169,34 +116,126 @@ namespace WurmSermoner
             this.tsLogFile.Size = new System.Drawing.Size(63, 20);
             this.tsLogFile.Text = "Logfile";
             // 
-            // txtList
+            // tabControl1
             // 
-            this.txtList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(1, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(645, 416);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnStuff);
+            this.tabPage1.Controls.Add(this.txtLogFile);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.txtLogsDir);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtOperator);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(637, 390);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Settings";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.txtList);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(637, 390);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "List";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btnStuff
+            // 
+            this.btnStuff.Location = new System.Drawing.Point(59, 81);
+            this.btnStuff.Name = "btnStuff";
+            this.btnStuff.Size = new System.Drawing.Size(83, 23);
+            this.btnStuff.TabIndex = 13;
+            this.btnStuff.Text = "Parse log";
+            this.btnStuff.UseVisualStyleBackColor = true;
+            // 
+            // txtLogFile
+            // 
+            this.txtLogFile.Location = new System.Drawing.Point(59, 55);
+            this.txtLogFile.Name = "txtLogFile";
+            this.txtLogFile.Size = new System.Drawing.Size(126, 20);
+            this.txtLogFile.TabIndex = 12;
+            this.txtLogFile.Text = "_Event.2022-01.txt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Logfile";
+            // 
+            // txtLogsDir
+            // 
+            this.txtLogsDir.Location = new System.Drawing.Point(59, 29);
+            this.txtLogsDir.Name = "txtLogsDir";
+            this.txtLogsDir.Size = new System.Drawing.Size(562, 20);
+            this.txtLogsDir.TabIndex = 10;
+            this.txtLogsDir.Text = "D:\\Games\\Steam\\steamapps\\common\\Wurm Online\\gamedata\\players\\Tiscattu\\logs\\";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Logs dir";
+            // 
+            // txtOperator
+            // 
+            this.txtOperator.Location = new System.Drawing.Point(59, 3);
+            this.txtOperator.Name = "txtOperator";
+            this.txtOperator.Size = new System.Drawing.Size(126, 20);
+            this.txtOperator.TabIndex = 8;
+            this.txtOperator.Text = "Tiscattu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Operator";
+            // 
+            // txtList
+            // 
+            this.txtList.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtList.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtList.Location = new System.Drawing.Point(0, 194);
+            this.txtList.Location = new System.Drawing.Point(3, 3);
             this.txtList.Multiline = true;
             this.txtList.Name = "txtList";
-            this.txtList.Size = new System.Drawing.Size(739, 223);
-            this.txtList.TabIndex = 10;
+            this.txtList.Size = new System.Drawing.Size(631, 384);
+            this.txtList.TabIndex = 11;
             // 
             // frmWurmSermoner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(739, 445);
-            this.Controls.Add(this.txtList);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.btnStop);
             this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.btnStuff);
-            this.Controls.Add(this.txtLogFile);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtLogsDir);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtOperator);
-            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "frmWurmSermoner";
@@ -204,26 +243,33 @@ namespace WurmSermoner
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmWurmSermoner_FormClosing);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtOperator;
-        private System.Windows.Forms.TextBox txtLogsDir;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtLogFile;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button btnStuff;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel tsBot;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.ToolStripStatusLabel tsLogFile;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnStuff;
+        private System.Windows.Forms.TextBox txtLogFile;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLogsDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOperator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TextBox txtList;
     }
 }
