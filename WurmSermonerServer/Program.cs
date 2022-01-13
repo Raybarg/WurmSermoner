687d49a97510eb628f746b4c97a8fb7387303b8d
Janne Papula <raybarg@gmail.com>
2022-01-13 16:03:05 +0200

    Comments away

--- WurmSermonerServer/Program.cs
+++ WurmSermonerServer/Program.cs
@@ -26,45 +26,6 @@ namespace WurmSermonerServer
                 Console.WriteLine(ASCIIEncoding.ASCII.GetString(data));
                 server.Send(data, data.Length, remote);
             }
-            /*
-            try
-            {
-                //---listen at the specified IP and port no.---
-                IPAddress localAdd = IPAddress.Parse("127.0.0.1");
-                TcpListener listener = new TcpListener(localAdd, 5000);
-                Console.WriteLine("Listening...");
-                listener.Start();
-                while (true)
-                {
-                    //---incoming client connected---
-                    TcpClient client = listener.AcceptTcpClient();
-
-                    //---get the incoming data through a network stream---
-                    NetworkStream nwStream = client.GetStream();
-                    byte[] buffer = new byte[client.ReceiveBufferSize];
-
-                    //---read incoming stream---
-                    int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
-
-                    //---convert the data received into a string---
-                    string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                    Console.WriteLine("Received : " + dataReceived);
-
-                    
-
-                    //---write back the text to the client---
-                    Console.WriteLine("Sending back : " + dataReceived);
-                    nwStream.Write(buffer, 0, bytesRead);
-                    client.Close();
-                }
-                listener.Stop();
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-            */
         }
     }
 }
